75a7af2f270261c0cfa0bbf07ff826707bf84b4b
Justin Loi <jmloi@hawaii.edu>
2023-06-10 14:07:08 -1000

    Logging Part Complete

--- main.py
+++ main.py
@@ -7,18 +7,71 @@
 ### @author Justin Loi
 ###############################################################################
 
-import requests
+#pip install websocket-client
+
+import config
+
+import websocket
 import json
+import threading
+import time
 
-def get_messages(channelid):
-    headers = {
-        # Key stored elsewhere
-        'authorization': 'test'
+def send_json_request(ws, request):
+    ws.send(json.dumps(request))
+
+def receive_json_response(ws):
+    response = ws.recv()
+    if response:
+        return json.loads(response)
+
+def heartbeat(interval, ws):
+    print('heartbeat begins')
+    while True:
+        time.sleep(interval)
+        heartbeatJSON = {
+            "op": 1,
+            "d": "null"
+        }
+        send_json_request(ws, heartbeatJSON)
+        print("heartbeat sent")
+
+ws = websocket.WebSocket()
+ws.connect('wss://gateway.discord.gg/?v=10&encoding=json')
+event = receive_json_response(ws)
+
+heartbeat_interval = event['d']['heartbeat_interval'] / 1000
+threading._start_new_thread(heartbeat, (heartbeat_interval, ws))
+
+
+token = config.test_server
+payload = {
+    'op': 2,
+    "d": {
+        "token": token,
+        "properties": {
+            "$os": "windows",
+            "$browser": "chrome",
+            "$device": "pc"
+        }
     }
-    r = requests.get(f'https://discord.com/api/v9/channels/{channelid}/messages', headers=headers)
-    json_object = json.loads(r.text)
-    for value in json_object:
-        print(value, '\n')
+}
 
-# Key stored elsewhere
-get_messages(test)
+send_json_request(ws, payload)
+
+while True:
+    event = receive_json_response(ws)
+
+    try:
+        print(f"{event['d']['author']['username']}: {event['d']['content']}")
+        op_code = event('op')
+        if op_code == 11:
+            print('heartbeat received')
+    except:
+        pass
+
+
+
+
+
+
+
